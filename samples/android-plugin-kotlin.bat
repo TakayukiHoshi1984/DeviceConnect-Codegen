43bf940b63389a4b5c258207845d970feb854d69
Takayuki Hoshi <hoshi@gclue.jp>
2020-05-13 12:43:13 +0900

    batに--template-dirを追加。

--- samples/android-plugin-kotlin.bat
+++ samples/android-plugin-kotlin.bat
@@ -4,6 +4,10 @@ set LANG=deviceConnectKotlinPlugin
 :: プロファイル定義ファイル
 set SPEC=.\sample-profile-specs\swagger.json
 
+
+:: テンプレート
+set TEMPLATE_DIR="./templates/deviceConnectKotlinPlugin"
+
 :: スケルトンコード出力先
 set OUTPUT_DIR=.\output\Android\MyPluginForKotlin
 
@@ -16,4 +20,4 @@ set DISPLAY_NAME=MyPluginForKotlin
 :: スケルトンコード生成ツールのバイナリ
 set JAR_FILE=..\bin\deviceconnect-codegen.jar
 
-java -Dfile.encoding=UTF-8 -jar %JAR_FILE% --config .\configs\android-plugin-kotlin.json --input-spec %SPEC%  --lang %LANG% --package-name %PACKAGE_NAME%  --display-name %DISPLAY_NAME%  --output %OUTPUT_DIR%
+java -Dfile.encoding=UTF-8 -jar %JAR_FILE% --config .\configs\android-plugin-kotlin.json --template-dir %TEMPLATE_DIR% --input-spec %SPEC%  --lang %LANG% --package-name %PACKAGE_NAME%  --display-name %DISPLAY_NAME%  --output %OUTPUT_DIR%
